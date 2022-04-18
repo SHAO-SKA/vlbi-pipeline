276e98e68d730c8801a73954b745a2cfc76c8911
Shaoguang Guo(Sky & Star) <sgguo@shao.ac.cn>
2022-04-18 19:05:35 +0800

    try to build the package

---
+++ vlbi-pipeline/__init__.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+"""
+-------------------
+vlbipipeline module.
+-------------------
+"""
+__author__ = 'GSG & ZYK & AT'
+__version__ = '0.1.0'
+__date__ = '2021-10-01'
+__institute__ = 'Shanghai Astronomical Observatory'
